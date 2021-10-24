b3d165b4c4441bc8891dc3ba8cfb3cafd9046701
Ana <ana.runje@live.com>
2021-10-24 00:53:06 +0000

    Feat: creat database model in models.py and register models in admin.py

--- planner/admin.py
+++ planner/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import MealPlan, MealType, Meal
+
 
 # Register your models here.
+@admin.register(MealPlan)
+class MealPlanAdmin(admin.ModelAdmin):
+    list_filter = ['user', 'start_date']
+    list_display = ['user', 'start_date']
+    search_fields = ['user']
+
+
+@admin.register(MealType)
+class MealTypeAdmin(admin.ModelAdmin):
+    list_display = ['meal_type_name']
+    list_filter = ['meal_type_name']
+
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ['meal_name', 'meal_plan', 'day', 'meal_type']
+    list_filter = ['meal_name', 'meal_plan', 'day', 'meal_type']
+    search_fields = ['meal_name', 'meal_plan', 'day', 'meal_type']
--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MealPlan(models.Model):
+    start_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class MealType(models.Model):
+    meal_type_name = models.CharField(max_length=50)
+
+
+class Meal(models.Model):
+    meal_name = models.CharField(max_length=50)
+    meal_plan = models.ForeignKey(MealPlan, on_delete=models.CASCADE)
+    day = models.DateField()
+    meal_type = models.ForeignKey(MealType, on_delete=models.CASCADE)
