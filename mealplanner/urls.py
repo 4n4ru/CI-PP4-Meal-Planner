47c5368fdfeec5d294b6fc19a494f7cab8b1c389
Ana <ana.runje@live.com>
2021-10-25 00:44:26 +0000

    Feat: Add basic templates and view to be able to load index.html

--- mealplanner/urls.py
+++ mealplanner/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('planner.urls'), name='planner_urls'),
     path('accounts/', include('allauth.urls')),
 ]
