f2c9d7c39c5c0e8fb01bc5421a6df5bb741dee6e
Ana <ana.runje@live.com>
2021-10-29 08:23:46 +0000

    Feat: Add Materialize CSS and set up basic table for meal plan

--- planner/urls.py
+++ planner/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Index.as_view(), name='home'),
+    path('add', views.AddMeal.as_view(), name='add_meal'),
+    path('pick_date', views.PickStartDate.as_view(), name='pick_start_date'),
 ]
