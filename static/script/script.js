b0d4dfddc781937329eff9e2ca14443ef4bb60cf
Ana <ana.runje@live.com>
2022-02-14 05:01:07 +0000

    Fix: Replace tooltip with materialize tooltip to correct placement

--- static/script/script.js
+++ static/script/script.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
 
+  // tooltip initialization
+  var tooltip = document.querySelectorAll('.tooltipped');
+  M.Tooltip.init(tooltip);
+
   // hide messages after 5 seconds
   setTimeout(() => {
     var messages = document.getElementsByClassName('messages')[0]
