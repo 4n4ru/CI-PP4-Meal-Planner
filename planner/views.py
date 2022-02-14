be6c008a248b4a6a8610994e85725ae943c1c7ba
Ana <ana.runje@live.com>
2022-02-14 01:54:13 +0000

    Fix: Fix pylint errors and add docstrings

--- planner/views.py
+++ planner/views.py
@@ -1,19 +1,41 @@
+# Imports:
+# 3rd party:
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View, generic
 from django.forms import formset_factory
 from django.contrib import messages
+
+# Internal
 from .forms import MealForm, MealPlanForm
 from .models import MealPlan, Meal, MealType
 
 
 class Index(View):
+    """
+    Displays the home page
+    """
     def get(self, request):
+        """Renders the home page
+        Args:
+            request (object): HTTP request object
+        Returns:
+            method: renders home page
+        """
         return render(request, 'index.html')
 
 
 class PickStartDate(LoginRequiredMixin, View):
+    """
+    A view for picking the start date for the mealplan
+    """
     def get(self, request):
+        """Renders the form to pick meal plan start date
+        Args:
+            request (object): HTTP request object
+        Returns:
+            method: renders pick_start_date.html
+        """
         return render(
             request,
             'pick_start_date.html',
@@ -23,6 +45,13 @@ class PickStartDate(LoginRequiredMixin, View):
         )
 
     def post(self, request):
+        """Takes input from meal_plan_form and if the form is valid
+        creates new meal plan else renders the form again
+        Args:
+            request (object): HTTP request object
+        Returns:
+            method: redirects to add meal view
+        """
         meal_plan_form = MealPlanForm(data=request.POST)
         if meal_plan_form.is_valid():
             user = request.user
@@ -46,7 +75,16 @@ class PickStartDate(LoginRequiredMixin, View):
 
 
 class AddMeal(View):
+    """
+    A view for adding meals into the meal plan
+    """
     def get(self, request):
+        """Renders the formset for adding meals to the meal plan
+        Args:
+            request (object): HTTP request object
+        Returns:
+            method: renders formset for adding meals
+        """
         try:
             meal_plan_id = request.session.get('meal_plan_id')
             meal_plan = MealPlan.objects.get(pk=meal_plan_id)
@@ -60,10 +98,18 @@ class AddMeal(View):
                     'formset': formset,
                 }
             )
-        except:
+        except Exception as error:
+            messages.error(request, f'An Error occured: {error}')
             return redirect('home')
 
     def post(self, request):
+        """Takes input from meal formset and if the formset is valid
+        creates new meals in database
+        Args:
+            request (object): HTTP request object
+        Returns:
+            method: redirects meal plans view
+        """
         meal_plan_id = request.session.get('meal_plan_id')
         meal_plan = MealPlan.objects.get(pk=meal_plan_id)
         days = [
@@ -81,7 +127,7 @@ class AddMeal(View):
             idx = 0
             for form in formset:
                 meal_name = form.cleaned_data.get('meal_name')
-                if meal_name == None:
+                if meal_name is None:
                     meal_name = ''
                 meal_day = days[idx % 7]
                 if idx < 7:
@@ -105,24 +151,43 @@ class AddMeal(View):
 
 
 class ViewMealPlans(LoginRequiredMixin, generic.list.ListView):
+    """
+    A view to display all meal plans of a user, paginated by 1
+    """
     model = MealPlan
     template_name = 'meal_plans.html'
     paginate_by = 1
 
     def get_queryset(self):
+        """
+        Returns:
+            list: list of all mealplans by a specific user orderd by start date
+        """
         return MealPlan.objects.filter(user=self.request.user).order_by(
             'day_1'
         )
 
 
 class EditMealPlan(LoginRequiredMixin, View):
-
+    """
+    A view for editing meal plans
+    """
     def get(self, request, **kwargs):
+        """Renders a formset prefilled with initial data for meal_plan_id
+        If the user requesting the edit is not the same as meal_plan.user
+        (user doesn't own the meal_plan) redirects to meal_plans view
+        Args:
+            request (object): HTTP request object
+            **meal_plan_id (int): pk of meal plan being edited
+        Returns:
+            method: renders edit_meal_plan form
+        """
         meal_plan_id = self.kwargs['meal_plan_id']
         meal_plan = MealPlan.objects.get(pk=meal_plan_id)
 
         if request.user == meal_plan.user:
-            # code on how to use inital data was inspired by this https://stackoverflow.com/a/15853036
+            # code on how to use inital data was inspired by this
+            # https://stackoverflow.com/a/15853036
             meals = Meal.objects.filter(meal_plan=meal_plan_id)
             meals_formset = formset_factory(MealForm, extra=21, max_num=21)
             initial_data = []
@@ -140,9 +205,16 @@ class EditMealPlan(LoginRequiredMixin, View):
             )
         else:
             messages.info(request, 'You can only edit your own mealplans')
-            return redirect('meal_plans')        
+            return redirect('meal_plans')
 
     def post(self, request, **kwargs):
+        """Takes input from meal formset and if the formset is valid
+        creates updates meals in database
+        Args:
+            request (object): HTTP request object
+        Returns:
+            method: redirects meal plans view
+        """
         meal_plan_id = self.kwargs['meal_plan_id']
         meal_plan = MealPlan.objects.get(pk=meal_plan_id)
         meals = Meal.objects.filter(meal_plan=meal_plan).order_by('pk')
@@ -164,8 +236,17 @@ class EditMealPlan(LoginRequiredMixin, View):
 
 
 class DeleteMealPlan(LoginRequiredMixin, View):
-
+    """
+    A view to delete meal plans
+    """
     def post(self, request, **kwargs):
+        """Deletes the sellected meal plan
+        Args:
+            request (object): HTTP request object
+            **meal_plan_id (int): pk of meal plan being deleted
+        Returns:
+            method: redirects to meal_plans
+        """
         meal_plan_id = self.kwargs['meal_plan_id']
         meal_plan = MealPlan.objects.get(pk=meal_plan_id)
         meal_plan.delete()
@@ -173,6 +254,11 @@ class DeleteMealPlan(LoginRequiredMixin, View):
         return redirect('meal_plans')
 
     def get(self, request, **kwargs):
+        """
+        Handles deleting by get method
+        Returns:
+            method: redircets to meal_plans
+        """
         messages.warning(
             request,
             'Deleting meal plans by URL is not allowed!'
